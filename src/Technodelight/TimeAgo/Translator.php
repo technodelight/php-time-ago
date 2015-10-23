fa833de85ae74c507f0265dd376b9a2027817db0
Zsolt Gál <zsolt.gal@sessiondigital.com>
2015-10-24 01:00:25 +0200

    Refactor switch/case as only one assertion is required per rule

--- src/Technodelight/TimeAgo/Translator.php
+++ src/Technodelight/TimeAgo/Translator.php
@@ -36,55 +36,46 @@ class Translator
             case $seconds <= $this->inSeconds('29sec'):
                 return $this->translation->text('lessThanAMinute');
 
-            case $seconds > $this->inSeconds('29sec')
-                && $seconds <= $this->inSeconds('1min 29sec'):
+            case $seconds <= $this->inSeconds('1min 29sec'):
                 return $this->translation->text('oneMinute');
 
-            case $seconds > $this->inSeconds('1min 29sec')
-                && $seconds <= $this->inSeconds('44min 29sec'):
+            case $seconds <= $this->inSeconds('44min 29sec'):
                 return sprintf(
                     $this->translation->text('lessThanOneHour'),
                     $this->secondsIn('min', $seconds)
                 );
 
-            case $seconds > $this->inSeconds('44min 29sec')
-                && $seconds <= $this->inSeconds('1hour 29min 59sec'):
+            case $seconds <= $this->inSeconds('1hour 29min 59sec'):
                 return $this->translation->text('aboutOneHour');
 
-            case $seconds > $this->inSeconds('1hour 29min 59sec')
-                && $seconds <= $this->inSeconds('23hour 59min 29sec'):
+            case $seconds <= $this->inSeconds('23hour 59min 29sec'):
                 return sprintf(
                     $this->translation->text('hours'),
                     $this->secondsIn('hour', $seconds)
                 );
 
-            case $seconds > $this->inSeconds('23hour 59min 29sec')
-                && $seconds <= $this->inSeconds('47hour 59min 29sec'):
+            case $seconds <= $this->inSeconds('47hour 59min 29sec'):
                 return $this->translation->text('aboutOneDay');
 
-            case $seconds > $this->inSeconds('47hour 59min 29sec')
-                && $seconds <= $this->inSeconds('29day 23hour 59min 29sec'):
+            case $seconds <= $this->inSeconds('29day 23hour 59min 29sec'):
                 return sprintf(
                     $this->translation->text('days'),
                     $this->secondsIn('day', $seconds)
                 );
 
-            case $seconds > $this->inSeconds('29day 23hour 59min 29sec')
-                && $seconds <= $this->inSeconds('59day 23hour 59min 29sec'):
+            case $seconds <= $this->inSeconds('59day 23hour 59min 29sec'):
                 return $this->translation->text('aboutOneMonth');
 
-            case $seconds > $this->inSeconds('59day 23hour 59min 29sec')
-                && $seconds < $this->inSeconds('1year'):
+            case $seconds < $this->inSeconds('1year'):
                 return sprintf(
                     $this->translation->text('months'),
                     $this->secondsIn('month', $seconds, 'ceil')
                 );
 
-            case $seconds >= $this->inSeconds('1year')
-                && $seconds < $this->inSeconds('2year'):
+            case $seconds < $this->inSeconds('2year'):
                 return $this->translation->text('aboutOneYear');
 
-            case $seconds >= $this->inSeconds('2year'):
+            default: // no need to check arithmetics here
                 return sprintf(
                     $this->translation->text('years'),
                     $this->secondsIn('year', $seconds, 'floor')
